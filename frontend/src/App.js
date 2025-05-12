ea143eb76d53712de124bf791899c1fb59a2b9e2
AbdulRiyan7892 <abdulriyan062@gmail.com>
2025-05-12 21:35:38 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";  
+import React, { useState, useEffect } from "react";   
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link, useNavigate } from "react-router-dom";
 import { FaHome, FaSignOutAlt } from "react-icons/fa";
